fa2b5e2d9fec5571ec336878f285694fd3d884c5
Zaw Htet Aung <zawhtetaung@amdon.com>
2021-12-02 11:29:59 +0630

    atlas

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/easy-notes",
+  url: "mongodb+srv://zawhtetaung:january199816@werkz.nqwvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
