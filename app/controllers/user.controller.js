1a83463226d6f5914e5277011df21d50fc73e0c3
Zaw Htet Aung <zawhtetaung@amdon.com>
2021-12-08 17:47:13 +0630

    email added

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,21 +2,29 @@ const User = require("../models/user.model.js");
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
   console.log("register log");
-
+  const token = await jwt.sign({ name: req.body.name, password: req.body.password }, "secret", {
+    expiresIn: "24h",
+  });
   // Register a User
   bcrypt.hash(req.body.password, 10).then(function (hash) {
     const user = new User({
       name: req.body.name,
       password: hash,
+      email: req.body.email,
     });
     // Save Note in the database
+
     user
       .save()
       .then((data) => {
         console.log("register then", data);
-        res.send(data);
+        res.send({
+          data: data,
+          token: token,
+          userId: user._id,
+        });
       })
       .catch((err) => {
         res.status(500).send({
